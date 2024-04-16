1f777c60a168dd9fc4b594a819f94753b4f80d32
unknown <saxiesluar7@gmail.com>
2024-04-16 00:18:41 -0600

    i started with te coding of the index

--- bd.php
+++ bd.php
@@ -4,6 +4,9 @@ $baseDeDatos="app";
 $usuario="root";
 $contraseña="";
 
-try{$conexion= new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario,$contraseña)catch(Exception $ex)}
+try{
+    $conexion= new PDO("mysql:host=$servidor;dbname=$baseDeDatos", $usuario,$contraseña);}catch(Exception $ex){
+        echo $ex->getMessage();      
+    }
 
 ?>
